74f045411d81b66c3952eb1633feda12b54ac846
ashishkr710 <77523570+ashishkr710@users.noreply.github.com>
2023-02-06 00:49:43 +0530

    emp

--- Table.java
+++ Table.java
@@ -2,17 +2,21 @@ import java.sql.*;
 
 public class Table {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/Java";
+        String url = "jdbc:mysql://localhost:3306/company";
         String username = "root";
         String password = "root";
 
         Connection con = null;
         try {
+
             Class.forName("java.sql.Driver");
+
             con = DriverManager.getConnection(url, username, password);
             Statement stmt = con.createStatement();
-            String sql = "INSERT INTO employee (empId, empName, deg, salarey) VALUES (365, 'Tare', 'Draver',98358)";
+            String sql = "INSERT INTO employee (empName,empSalary) VALUES ('Shivam',9999954)";
             int x=stmt.executeUpdate(sql);
+
+            con.close();
 
             if(x>0)
                 System.out.println("Insertion Successful");
@@ -21,6 +25,7 @@ public class Table {
         } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
+
 
     }
 
---
+++ out/production/Verify.java
@@ -0,0 +1,9 @@
+package out.production;
+
+public class Verify {
+	
+ public static void main(String[] args) {
+	System.out.println("Working");
+}
+
+}
