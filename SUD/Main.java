d5e6861e13e7e9ba517e491cb12e3ada2ccb633c
ashishkr710 <77523570+ashishkr710@users.noreply.github.com>
2023-02-12 22:56:09 +0530

    update

--- SUD/Main.java
+++ SUD/Main.java
@@ -17,9 +17,26 @@ public class Main {
         int choice = Integer.parseInt(br.readLine());
 
         switch (choice) {
-            case 1 ->
+            case 1 ->{
                 //Search Employee
-                    System.out.println("Search");
+                     System.out.println("Search");
+                    System.out.print("Enter Employee Id = ");
+                    int empId =Integer.parseInt(br.readLine());
+                    Employee ep = new Employee(empId);
+                    Connection con = Connect();
+                    String q = "SELECT * FROM employee  WHERE empId="+empId;
+                    Statement stmt = con.createStatement();
+                    ResultSet set = stmt.executeQuery(q);
+                    set.next();
+                    int id = set.getInt(1);
+                    String name = set.getString(2);
+                    int Salary = set.getInt(3);
+
+                System.out.println("ID: "+id);
+                System.out.println("name: "+name);
+                System.out.println("Salary:"+Salary);
+                con.close();
+            }
             case 2 -> {
                 //ADD New Employee
                 System.out.println("ADD");
@@ -35,13 +52,45 @@ public class Main {
                 stmt.setInt(2, ep.getEmpSalary());
                 stmt.executeUpdate();
                 System.out.println(ep);
+                con.close();
             }
-            case 3 ->
+            case 3 ->{
                 // Update Employee
                     System.out.println("Update");
-            case 4 ->
+                System.out.print("Enter Employee Id to Update : ");
+                int empId =Integer.parseInt(br.readLine());
+                System.out.print("Enter Updated Employee Name : ");
+                String empName = br.readLine();
+                System.out.print("Enter Updated Employee Salary : ");
+                int empSalary = Integer.parseInt(br.readLine());
+                Employee ep = new Employee(empId,empName,empSalary);
+                Connection con = Connect();
+//                String q = "update employee set (empName,empSalary) VALUES (?,?) where empId=?";
+                String q = "update employee set empName = ?, empSalary = ? where empId = ?";
+                PreparedStatement stmt = con.prepareStatement(q);
+                stmt.setString(1, ep.getEmpName());
+                stmt.setInt(2,ep.getEmpSalary());
+                stmt.setInt(3,ep.getEmpId());
+                stmt.executeUpdate();
+                System.out.println(ep);
+                con.close();
+            }
+            case 4 ->{
                 // Delete Employee
-                    System.out.println("Delete");
+                    System.out.println("Enter Employee Id to Delete");
+                    int empId =Integer.parseInt(br.readLine());
+                    Employee ep = new Employee(empId);
+                    Connection con = Connect();
+                    String q = "DELETE FROM employee WHERE empId=?";
+                    PreparedStatement stmt = con.prepareStatement(q);
+                    stmt.setInt(1, ep.getEmpId());
+                    stmt.executeUpdate();
+                    System.out.println(ep);
+                    con.close();
+            }
+
+
+
         }
 
     }
@@ -65,6 +114,10 @@ public class Main {
 
         public Employee() {
             super();
+        }
+
+        public Employee(int empId) {
+            this.empId = empId;
         }
 
         public int getEmpId() {
