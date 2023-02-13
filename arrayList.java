0b034f291e6aa94e4fd4f89e22ec9b8365ddadba
ashishkr710 <77523570+ashishkr710@users.noreply.github.com>
2023-02-13 12:13:47 +0530

    arrayList

--- arrayList.java
+++ arrayList.java
@@ -1,6 +1,3 @@
-
-import SUD.Main;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -10,7 +7,7 @@ import java.util.ArrayList;
 
     public class arrayList {
         public static void main(String[] args) {
-            ArrayList<String> Employee = new ArrayList<String>();
+            ArrayList<Employee> Employee = new ArrayList<>();
             Connection connection = null;
             Statement statement = null;
             ResultSet resultSet = null;
@@ -21,21 +18,21 @@ import java.util.ArrayList;
             String password = "root";
 
             try {
-                // Load the JDBC driver
                 Class.forName("java.sql.Driver");
 
-                // Establish a connection
                 connection = DriverManager.getConnection(url, username, password);
 
-                // Create a statement
                 statement = connection.createStatement();
 
-                // Execute the query
                 resultSet = statement.executeQuery("SELECT * FROM employee");
 
-                // Iterate through the result set and add each name to the ArrayList
                 while (resultSet.next()) {
-                    Employee.add(resultSet.getString(username));
+//                    Employee.add(resultSet.getString("empName"));;
+                    int id = resultSet.getInt("empId");
+                    String name = resultSet.getString("empName");
+                    int salary = resultSet.getInt("empSalary");
+                    // Add the employee object to the ArrayList
+                    Employee.add(new Employee(id,name,salary));
                 }
 
             } catch (SQLException | ClassNotFoundException e) {
@@ -56,8 +53,53 @@ import java.util.ArrayList;
                 }
             }
 
-            // Print the contents of the ArrayList
-            for (String name : Employee) {
+            for (arrayList.Employee name : Employee) {
                 System.out.println(name);}
+        }
+        public static class Employee{
+            private int empId;
+            private String empName;
+            private int empSalary;
+            public Employee(int empId, String empName, int empSalary) {
+                this.empId = empId;
+                this.empName = empName;
+                this.empSalary = empSalary;
+            }
+            public Employee(String empName, int empSalary) {
+                this.empName = empName;
+                this.empSalary = empSalary;
+            }
+            public Employee() {
+                super();
+            }
+            public Employee(int empId) {
+                this.empId = empId;
+            }
+            public int getEmpId() {
+                return empId;
+            }
+            public void setEmpId(int empId) {
+                this.empId = empId;
+            }
+            public String getEmpName() {
+                return empName;
+            }
+            public void setEmpName(String empName) {
+                this.empName = empName;
+            }
+            public int getEmpSalary() {
+                return empSalary;
+            }
+            public void setEmpSalary(int empSalary) {
+                this.empSalary = empSalary;
+            }
+            @Override
+            public String toString() {
+                return "Employee[" +
+                        "empId=" + empId +
+                        ", empName='" + empName + '\'' +
+                        ", empSalary=" + empSalary +
+                        ']';
+            }
         }
     }
